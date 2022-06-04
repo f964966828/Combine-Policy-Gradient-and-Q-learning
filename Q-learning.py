4df31997e0fbee8b0de921605aa55eaefdcd5636
f964966828 <88909835+f964966828@users.noreply.github.com>
2022-06-04 23:07:06 +0800

    Add files via upload

--- Q-learning.py
+++ Q-learning.py
@@ -25,6 +25,7 @@ def get_args():
     parser.add_argument('--hidden', default=256, type=int, help='hidden size of GRU')
     parser.add_argument('--capacity', default=10000, type=int, help='capacity for replay buffer')
     parser.add_argument('--batch_size', default=32, type=int, help='batch size for replay buffer')
+    parser.add_argument('--freq', default=10, type=int, help='update frequency for replay buffer')
     parser.add_argument('--alpha', default=.1, type=float, help='parameter of entropy regularization')
     return parser.parse_args()
 
